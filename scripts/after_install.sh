8bd93b414538e5f2d01da1a3ecf0aa62617681ac
wachiye <160839976+wachiyel@users.noreply.github.com>
2024-09-29 15:22:55 +0300

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -6,5 +6,12 @@ cd /app
 # install dependencies
 npm install
 npm run build
+# Ensure the build directory exists and copy build files to the Nginx root directory
+#rm -rf /var/www/html/* # Remove existing files in Nginx root directory
 cp -r build/* /var/www/html
-npm install pm2 -g
+
+# Install PM2 globally (only if you're running a Node.js backend, otherwise this can be omitted)
+# npm install pm2 -g
+
+# Restart Nginx to serve the updated files
+systemctl restart nginx
